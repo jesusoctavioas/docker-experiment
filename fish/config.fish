0921be962253c4c0fa7072b35d1f6e4a701e03bf
Henrique Borges <henriqueborgeshbr@gmail.com>
2022-02-14 16:44:31 -0300

    Remove unnecessary container verification

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,9 @@
-fish_add_path /root/.local/bin
+set containerized_tools (find $HOME/repos -mindepth 1 -maxdepth 1 -not -path "*/.*" -type d -exec basename {} \;)
 
-if is-docker-container
-  set containerized_tools (find $HOME/repos -mindepth 1 -maxdepth 1 -not -path "*/.*" -type d -exec basename {} \;)
-
-  # these aliases are required for running containerized tools just specifying their names
-  # $HOSTNAME becomes the container id when inside a docker container
-  for tool in $containerized_tools;
-    alias $tool "docker run --rm -w \$PWD --volumes-from \$HOSTNAME $tool"
-  end
+# these aliases are required for running containerized tools just specifying their names
+# $HOSTNAME becomes the container id when inside a docker container
+for tool in $containerized_tools;
+  alias $tool "docker run --rm -w \$PWD --volumes-from \$HOSTNAME $tool"
 end
 
 alias dots "git --git-dir=$HOME/repos/dots/.git --work-tree=$HOME/repos/dots"
